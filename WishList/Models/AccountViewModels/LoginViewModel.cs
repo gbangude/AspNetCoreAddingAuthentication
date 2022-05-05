14e0cdb68843431ae5c995a2c871dd4afa683182
gbangude <gayatri.bangude@gmail.com>
2022-05-05 13:06:45 +0000

    done

---
+++ WishList/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public String Password { get; set; }
+        
+
+    }
+}
---
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace WishList.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength =8)]
+        [DataType(DataType.Password)]
+        public  String Password{ get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password do not match")]
+        public String ConfirmPassword { get; set; }
+    }
+}
